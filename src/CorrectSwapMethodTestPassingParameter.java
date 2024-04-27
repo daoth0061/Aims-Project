4cd6b7ceb042db1598448154a398096f5f93f4f0
daoth0061 <tranhuudao08@gmail.com>
2024-04-27 10:39:34 +0700

    Complete topic/passing-parameter

---
+++ src/CorrectSwapMethodTestPassingParameter.java
@@ -0,0 +1,22 @@
+public class CorrectSwapMethodTestPassingParameter {
+    public static void swap(ObjectWrapper a, ObjectWrapper b) {
+        Object tmp = a.obj;
+        a.obj = b.obj;
+        b.obj = tmp;
+    }
+    public static void main(String[] args) {
+        DigitalVideoDisc jungleDVD = new DigitalVideoDisc("Jungle");
+        DigitalVideoDisc cinderellaDVD = new DigitalVideoDisc("Cinderella");
+
+        ObjectWrapper ow1 = new ObjectWrapper(jungleDVD);
+        ObjectWrapper ow2 = new ObjectWrapper(cinderellaDVD);
+        swap(ow1, ow2);
+        // Now ow1.obj holds cinderellaDVD and ow2.obj holds jungleDVD
+        jungleDVD = (DigitalVideoDisc) ow1.obj;
+        cinderellaDVD = (DigitalVideoDisc) ow2.obj;
+
+        System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+        System.out.println("cinderella dvd title: " + cinderellaDVD.getTitle());
+
+    }
+}
