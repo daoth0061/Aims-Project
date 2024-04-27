08f82d8b9a814a6f295e945dd3a880fd110748b7
daoth0061 <tranhuudao08@gmail.com>
2024-04-27 17:04:00 +0700

    Complete topic/print-seach-cart

---
+++ src/CartTest.java
@@ -0,0 +1,24 @@
+public class CartTest {
+    public static void main(String[] args) {
+        Cart cart = new Cart();
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King",
+                "Animation", "Roger Allers", 87, 19.95f);
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars",
+                "Science Fiction", "George Lucas", 87, 24.95f);
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin",
+                "Animation", 18.99f);
+        cart.addDigitalVideoDisc(dvd1);
+        cart.addDigitalVideoDisc(dvd2);
+        cart.addDigitalVideoDisc(dvd3);
+        // Test the print method
+        cart.print();
+        // Test the search method
+        cart.searchDVDByID(0);
+        cart.searchDVDByID(2);
+        cart.searchDVDByID(3);
+
+        cart.searchDVDByTitle("The Lion King");
+        cart.searchDVDByTitle("Aladin");
+        cart.searchDVDByTitle("Uns");
+    }
+}
